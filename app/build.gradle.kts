7bb855c6c2ea210e425f35c9dbb4ad6acd6e60ee
Omar Titouhi <titouhiomar223@gmail.com>
2026-05-16 22:54:25 +0100

    Bump version name to 1.0.1 and update deployment target selection timestamp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val mindMateApiBaseUrl = providers.gradleProperty("MINDMATE_API_BASE_URL")
